ae974a0b84f99bdb02319f3f8996f7ab8bd49b44
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-17 13:00:47 -0700

    Creacion de la clase Tecnicos en la carpeta Models

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTenicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Concepto { get; set; } = null!;
+    }
+}
