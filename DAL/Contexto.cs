16f5a899f8d937e8d14a3788c2ebb4ca9db69573
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-17 13:11:56 -0700

    Creacion de la clase de Contexto en la carpeta DAL mas la instalacion del Nuget Npgsql.EntityFrameworkCore.PostgreSQL

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using RegistrosTenicos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistrosTenicos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
