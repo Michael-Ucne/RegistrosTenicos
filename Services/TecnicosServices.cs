c7583e0ae997afbae730cd74498cad20d6e3dad7
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-17 14:33:11 -0700

    Creacion del Metodo Existe

--- Services/TecnicosServices.cs
+++ Services/TecnicosServices.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistrosTenicos.DAL;
+using RegistrosTenicos.Models;
 
 namespace RegistrosTenicos.Services
 {
     public class TecnicosServices(IDbContextFactory<Contexto> DbFactory)
     {
 
+        // Metodo Existe
+        private async Task<bool> Existe(int tecnicoId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tecnicos
+                .AnyAsync(t => t.TecnicoId == tecnicoId);
+        }
     }
 }
