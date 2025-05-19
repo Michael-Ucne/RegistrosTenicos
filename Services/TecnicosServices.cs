b5a42a5293a5ad317200777f497785b03b767718
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-19 07:25:37 -0700

    He realizado cambio en el componente TecnicoCreate cambie tecnicoexiste por mensaje, En el componente Tecnico he realizado el mismo cambio que en el TecnicoCreate pero he agregado el boton de retrocede y el TecnicoServices en el metodo de buscar he eliminado una linea de codigo innecesaria

--- Services/TecnicosServices.cs
+++ Services/TecnicosServices.cs
@@ -50,7 +50,7 @@ namespace RegistrosTenicos.Services
         public async Task<Tecnicos?> Buscar(int tecnicoId)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
-            return await contexto.Tecnicos.Include(d => d.TecnicoId)
+            return await contexto.Tecnicos
                 .FirstOrDefaultAsync(t => t.TecnicoId == tecnicoId);
         }
 
