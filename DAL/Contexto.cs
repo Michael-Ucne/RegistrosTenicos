ac68b7b9885f5eaed8a075088b4821a2e3179f64
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-20 05:38:37 -0700

    Corregi TecnicoID por Tecnico a ID en los componentes TecnicoIndex, TecnicoCreate y TecnicoEdit

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,11 @@
 ﻿using RegistrosTenicos.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace RegistrosTenicos.DAL
-{
-    public class Contexto : DbContext
-    {
-        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+namespace RegistrosTenicos.DAL;
 
-        public DbSet<Tecnicos> Tecnicos { get; set; }
-    }
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Tecnicos> Tecnicos { get; set; }
 }
