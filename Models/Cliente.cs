63a8d19bb032447ce1cff5dc4968a3f7672d210b
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-24 06:31:29 -0700

    Creacion de Models Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTenicos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La Fecha de Ingreso es requerida")]
+        [DataType(DataType.Date)]
+        public DateTime FechaIngreso { get; set; }
+
+        [Required(ErrorMessage = "La Dirección es requerida")]
+        public string Direccion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El RNC es requerido")]
+        public string RNC { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Límite de Crédito es requerido")]
+        public double LimiteCredito { get; set; }
+
+        [Required(ErrorMessage = "El Técnico Encargado es requerido")]
+        public int TecnicoEncargadoId { get; set; }
+    }
+}
---
+++ Services/ClientesServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosTenicos.DAL;
+using RegistrosTenicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistrosTenicos.Services
+{
+    public class ClientesServices(IDbContextFactory<Contexto> DbFactory)
+    {
+
+    }
+}
