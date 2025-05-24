6998638f089bcb30bb1dfe6b18eb2036d84ed0db
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-05-24 05:55:08 -0700

    Creacion de Routers estaticos y aplicacion inmediata en los componentes Tecnicos

---
+++ Components/Others/AppRoutes.cs
@@ -0,0 +1,16 @@
+﻿namespace RegistrosTenicos.Components.Others
+{
+    public class AppRoutes
+    {
+        // Routers de Tecnicos
+        public static string TIndex => "/Tecnico";
+        public static string TCreate => "/Tecnico/Create";
+        public static string TEdit => "/Tecnico/Edit";
+        public static string TDelete => "/Tecnico/Delete";
+
+        // Routers de Clientes
+        public static string CIndex => "/Cliente";
+        public static string CCraete => "/Cliente/Create";
+        public static string CEdit => "/Cliente/Edit";
+    }
+}
