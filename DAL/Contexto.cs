6c1375ff3b2a8c20f31edad89202f5a9be0ebd92
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-14 12:09:15 -0400

    Agrega el Contexto de Sistema

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,5 @@ public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
     public DbSet<Tecnicos> Tecnicos { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
     public DbSet<Tickets> Tickets { get; set; }
-
+    public DbSet<Sistema> Sistema { get; set; }
 }
