72a8247eeb0f716d7906014f8102b035e66a91c7
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-14 12:10:45 -0400

    Agrega el metodo Existe

--- Services/SistemasServices.cs
+++ Services/SistemasServices.cs
@@ -1,6 +1,15 @@
-﻿namespace RegistrosTenicos.Services
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosTenicos.DAL;
+
+namespace RegistrosTenicos.Services
 {
-    public class SistemasServices
+    public class SistemasServices(IDbContextFactory<Contexto> DbFactory)
     {
+        private async Task<bool> Existe(int sistemaId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Sistema
+                .AnyAsync(t => t.SistemaId == sistemaId);
+        }
     }
 }
