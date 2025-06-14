1074c16f78afdd1d5aa9dd5d54bfdd8090cf59c8
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-14 12:59:47 -0400

    Crea la tabla de Sistema en la base de datos

---
+++ Migrations/20250614165746_Crea la tabla de Sistema.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RegistrosTenicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrealatabladeSistema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tickets",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Sistema",
+                columns: table => new
+                {
+                    SistemaId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Complejidad = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sistema", x => x.SistemaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sistema");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tickets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
