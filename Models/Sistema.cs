5f19fcf701736d68eb387d471bf58530a7b0c7b8
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-14 12:08:28 -0400

    Agrega el modelo de sistema

--- Models/Sistema.cs
+++ Models/Sistema.cs
@@ -1,6 +1,16 @@
-﻿namespace RegistrosTenicos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosTenicos.Models
 {
     public class Sistema
     {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "La Descripcion es Requerida")]
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = "La Complejidad es Requerida")]
+        public double Complejidad { get; set; }
     }
 }
